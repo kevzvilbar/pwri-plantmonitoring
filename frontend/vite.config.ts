4008aa0e698b7338f152c0f9d2b2a1803fb43ef5
kevzvilbar <kevzvilbar@gmail.com>
2026-04-30 05:08:57 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -29,7 +29,6 @@ export default defineConfig(({ mode }) => ({
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
-        },
       },
     },
   },
